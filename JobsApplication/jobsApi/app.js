293102b94e9ba4749a8a144d59202512ce6fce65
sachchin06 <sachchin06@gmail.com>
2023-03-11 11:10:08 +0530

    files updated

--- JobsApplication/jobsApi/app.js
+++ JobsApplication/jobsApi/app.js
@@ -26,7 +26,10 @@ app.use("/api/v1/jobs", authenticateUser, jobsAwsRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-connectDB(process.env.MONGO_URI);
+// connectDB(process.env.MONGO_URI);
+connectDB(
+  "mongodb+srv://Sachchin06:ceylonuni@cluster0.jiqdoh2.mongodb.net/Jobs-Crud?retryWrites=true&w=majority"
+);
 console.log("DB Connected ...");
 
 module.exports = app;
